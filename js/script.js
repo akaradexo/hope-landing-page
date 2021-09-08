c139cfa28f145f91420f6f12f388e2f0ae4972da
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-09-08 11:46:31 +0530

    all done

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// theme changer
+		var two=document.getElementById('two');
+		two.addEventListener('click',function(){
+			var body=document.querySelector('body');
+      var sayingHead = document.getElementById('sayingHead').innerHTML="Hope is a waking dream.";
+      var sayingQuote =document.getElementById('sayingQuote').innerHTML="its is being able to see that there is light despite all of the darkness.";
+			body.classList.add('active1');
+			body.classList.remove('active2');
+		})
+		var one=document.getElementById('one');
+		one.addEventListener('click',function(){
+			var body=document.querySelector('body');
+      var sayingHead = document.getElementById('sayingHead').innerHTML="Never lose your faith.";
+      var sayingQuote =document.getElementById('sayingQuote').innerHTML="its's often in the darkest moments where you find clarity,hope and motivation to overcome.";
+			body.classList.remove('active1');
+			body.classList.remove('active2');
+
+		})
+		var three=document.getElementById('three');
+		three.addEventListener('click',function(){
+			var body=document.querySelector('body');
+      var sayingHead = document.getElementById('sayingHead').innerHTML="Love keeps you alive.";
+      var sayingQuote =document.getElementById('sayingQuote').innerHTML="there is  only one happiness in life to love or to be loved.";
+			body.classList.add('active2');
+			body.classList.remove('active1'); 
+
+		})
+
+//mobile menu
+function toggleMenu() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
